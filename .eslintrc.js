a464c350a40b5d9fb8492494590e061ff5d4234d
Jhonatan <1164@holbertonschool.com>
2021-05-20 00:53:31 -0500

    add lint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true
+	},
+	'parserOptions': {
+		'ecmaVersion': 12
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
