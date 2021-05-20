cb23d2416fd5c6d2fc864e53060b3fd09b07b01c
Jhonatan <1164@holbertonschool.com>
2021-05-20 01:10:03 -0500

    fix

--- routes/api/v1/portfolio.js
+++ routes/api/v1/portfolio.js
@@ -2,14 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 /** GET portfolio. 
- * This route returns all aspirants saved in the db
+ * This route returns all information of the first portfolio saved on database
+ * response's body:
+ * {
+ * //TODO check response resource
+ *  status: 'ok',
+ *  data: [aspirants]
+ * }
+*/
+router.get('/portfolio', function (req, res) {
+	res.status(200).json('to implement');
+});
+
+/** GET portfolio. 
+ * This route allows to modifed the first portfolio saved on database
+ * //TODO check response resource
+ * //TODO check request validation
  * response's body:
  * {
  *  status: 'ok',
  *  data: [aspirants]
  * }
 */
-router.get('/portfolio', function (req, res) {
+router.patch('/portfolio/', function (req, res) {
 	res.status(200).json('to implement');
 });
 
--- tests/api/v1/portfolio.test.js
+++ tests/api/v1/portfolio.test.js
@@ -1,10 +1,18 @@
 const request = require('supertest');
 
 const app = require('../../../index').plain_app;
-describe('Post Endpoints', () => {
-	it('should create a new post', async () => {
+describe('Get Endpoint', () => {
+	it('should exists the endpoint', async () => {
 		const res = await request(app)
 			.get('/api/v1/portfolio');
+		expect(res.statusCode).toEqual(200);
+	});
+});
+
+describe('Patch Endpoint', () => {
+	it('should exists the endpoint', async () => {
+		const res = await request(app)
+			.patch('/api/v1/portfolio');
 		expect(res.statusCode).toEqual(200);
 	});
 });
